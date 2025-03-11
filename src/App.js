a0a272c0456f7a972cf84592ee4d032a8d326786
MarmoW <115847012+MarmoW@users.noreply.github.com>
2025-03-11 16:14:35 -0300

    Feat: Header start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header, Button1, GlobalStyles, LogoImg, Title } from './components/styles';
+import { AiFillInstagram } from "react-icons/ai";
+import Logo from "./Logo-acobom.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles></GlobalStyles>
+      <Header>
+        <LogoImg  src={Logo}/>
+        <div>
+        <Button1>Máquinas</Button1>
+        <Button1>Telhas</Button1>
+        <Button1>Ferragens</Button1>
+        <Button1>Fale Conosco</Button1>
+        <Button1>Colaboradores</Button1>
+        <Button1><AiFillInstagram size={25}/><a>Instagram</a></Button1>
+        </div>
+      </Header>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/styles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+const Header = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+background-color: #515151;
+width: 100%;
+height: 6em;
+padding-left:4em;
+padding-right: 4em;
+
+`;
+
+const Button1 = styled.button`
+justify-content: center;
+align-items: center;
+background-color:#515151;
+max-height: 100%;
+margin: 2em;
+color:#ffffff;
+padding:0;
+
+
+&:hover {
+    color:#737373;
+  }
+
+
+`
+
+const LogoImg = styled.img`
+position: relative;
+display: flex;
+max-height: 3em;
+
+`;
+
+const Title = styled.a`
+display: flex;
+justify-contet: center;
+color: #fe7100
+`;
+
+const GlobalStyles = createGlobalStyle`
+  /* Reset básico */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  /* Melhorias na tipografia */
+  html {
+    font-size: 16px;
+    font-family: Arial, sans-serif;
+  }
+
+  /* Remover decoração de links */
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  /* Remover estilos padrão de botões */
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    font-family: inherit;
+  }
+
+  /* Estilização do body */
+  body {
+    background: #f5f5f5;
+    color: #333;
+  }
+`;
+
+export {Header, Button1, GlobalStyles, LogoImg, Title}; 
