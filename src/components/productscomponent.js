7bb8300794f6b2c4349bb7946ab1fcd43b171178
MarmoW <115847012+MarmoW@users.noreply.github.com>
2025-03-14 16:13:22 -0300

    Feat: ProductsData folder

--- src/components/productscomponent.js
+++ src/components/productscomponent.js
@@ -1,37 +1,6 @@
 import styled from "styled-components";
+import {products} from "../products-data/productsData";
 
-const products = [
-    {
-      name: "Product 1",
-      image: "/imgs/mock-products1.jpg", 
-      description: "This is the description for Product 1.",
-    },
-    {
-      name: "Product 2",
-      image: "/imgs/mock-products1.jpg", 
-      description: "This is the description for Product 2.",
-    },
-    {
-      name: "Product 3",
-      image: "/imgs/mock-products1.jpg", 
-      description: "This is the description for Product 3.",
-    },
-    {
-      name: "Product 4",
-      image: "/imgs/mock-products1.jpg", 
-      description: "This is the description for Product 4.",
-    },
-    {
-      name: "Product 5",
-      image: "/imgs/mock-products1.jpg", 
-      description: "This is the description for Product 5.",
-    },
-    {
-      name: "Product 6",
-      image: "/imgs/mock-products1.jpg", 
-      description: "This is the description for Product 6.",
-    },
-];
 
 const OtherProductsDiv = styled.div`
 display: flex; 
---
+++ src/products-data/productsData.js
@@ -0,0 +1,34 @@
+ const products = [
+    {
+      name: "Product 1",
+      image: "/imgs/mock-products1.jpg", 
+      description: "This is the description for Product 1.",
+    },
+    {
+      name: "Product 2",
+      image: "/imgs/mock-products1.jpg", 
+      description: "This is the description for Product 2.",
+    },
+    {
+      name: "Product 3",
+      image: "/imgs/mock-products1.jpg", 
+      description: "This is the description for Product 3.",
+    },
+    {
+      name: "Product 4",
+      image: "/imgs/mock-products1.jpg", 
+      description: "This is the description for Product 4.",
+    },
+    {
+      name: "Product 5",
+      image: "/imgs/mock-products1.jpg", 
+      description: "This is the description for Product 5.",
+    },
+    {
+      name: "Product 6",
+      image: "/imgs/mock-products1.jpg", 
+      description: "This is the description for Product 6.",
+    },
+];
+
+export {products};
